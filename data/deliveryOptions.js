e22f9f01baa1d1b9df2e8cbb392ac103e7ef2017
chakri <bandichakri15@gmail.com>
2025-09-18 16:38:53 +0000

    Save data and generate HTML for delivary options

---
+++ data/deliveryOptions.js
@@ -0,0 +1,17 @@
+export const deliveryOptions = [
+  {
+    id: "1",
+    delivaryDate: 7,
+    priceCents: 0,
+  },
+  {
+    id: "2",
+    delivaryDate: 3,
+    priceCents: 499,
+  },
+  {
+    id: "3",
+    delivaryDate: 1,
+    priceCents: 999,
+  },
+];
