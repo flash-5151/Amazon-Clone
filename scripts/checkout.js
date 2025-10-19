2632d09195fc711584257e879142d35a55ba4c1a
chakri <bandichakri15@gmail.com>
2025-10-19 19:03:52 +0000

    create class version of Cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,7 +1,7 @@
 import { renderOrderSummery } from "./checkout/orderSummary.js";
 import { renderPaymentSummery } from "./checkout/paymentSummery.js";
 import { renderHeader } from "./checkout/header.js";
-import "../data/cart-oop.js";
+import "../data/cart-class.js";
 
 renderHeader();
 renderOrderSummery();
